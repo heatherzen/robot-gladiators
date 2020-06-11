f30b24c1b47eb79bfdb2ad411861e769f7327dcb
heatherzen <heather.zen@gmail.com>
2020-06-10 22:31:19 -0700

    added arrays and arguments

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,11 +6,12 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyNames) {
+  //fight function statements
   // Alert users that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
@@ -22,25 +23,25 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   // remove enemy's health by subtracting the amount set in the playerAttack variable
   enemyHealth = enemyHealth - playerAttack;
   console.log(
-    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
   );
 
   // check enemy's health
   if (enemyHealth <= 0) {
-    window.alert(enemyName + " has died!");
+    window.alert(enemyNames + "has died!");
   } else {
-    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    window.alert(enemyNames + " still has " + enemyHealth + " health left.");
   }
 
   // remove player's health by subtracting the amount set in the enemyAttack variable
   playerHealth = playerHealth - enemyAttack;
   console.log(
-    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
   );
 
   // check player's health
   if (playerHealth <= 0) {
-    window.alert(playerName + " has died!");
+    window.alert(playerName + "has died!");
   } else {
     window.alert(playerName + " still has " + playerHealth + " health left.");
   }
@@ -61,4 +62,6 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   }
 }
 }
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
